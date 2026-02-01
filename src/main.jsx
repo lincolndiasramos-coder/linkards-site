c5477895eb67a0d48f77ac6ed927cde2f62c5662
lincolndiasramos-coder <lincolndiasramos@gmail.com>
2026-02-01 03:21:35 -0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-
-function App() {
-  return (
-    <div style={{ padding: "20px", fontFamily: "Arial" }}>
-      <h1>LinCards</h1>
-      <p>App carregando com sucesso 🚀</p>
-    </div>
-  );
-}
+import App from "./App";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
